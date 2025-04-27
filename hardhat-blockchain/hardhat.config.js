dccc19c04e704f178a6c4a9680434b4263f3679f
Quan Pham <qmpham2019@gmail.com>
2025-04-27 12:05:38 -0400

    Updated hardhat config

--- hardhat-blockchain/hardhat.config.js
+++ hardhat-blockchain/hardhat.config.js
@@ -1,10 +1,46 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
+require('hardhat-resolc');
+require('hardhat-revive-node');
+
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.20",
-    networks: {
-        localhost: {
-            url: "http://127.0.0.1:8545"
-        }
-    }
+  solidity: '0.8.28',
+  resolc: {
+    compilerSource: 'binary',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 400,
+      },
+      evmVersion: 'istanbul',
+      compilerPath: '/Users/quanmp/Documents/Hackathon/resolc-universal-apple-darwin',
+      standardJson: true,
+    },
+  },
+  networks: {
+    hardhat: {
+      polkavm: true,
+      nodeConfig: {
+        nodeBinaryPath: '/Users/quanmp/Documents/Hackathon/polkadot-sdk/target/release/substrate-node',
+        rpcPort: 8000,
+        dev: true,
+      },
+      adapterConfig: {
+        adapterBinaryPath: '/Users/quanmp/Documents/Hackathon/polkadot-sdk/target/release/eth-rpc',
+        dev: true,
+      },
+    },
+    localNode: {
+      polkavm: true,
+      url: `http://127.0.0.1:8545`,
+    },
+    westendAssetHub: {
+      polkavm: true,
+      url: 'https://westend-asset-hub-eth-rpc.polkadot.io',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
